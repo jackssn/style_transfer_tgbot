b6bfa69272953a7b187f506941b346c972a0ae33
jackssn <jackssn@github.com>
2020-06-24 00:30:20 +0300

    Init project

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# style_transfer_tgbot
+# Style transfer telegram bot
+
+Telegram bot using Heroku and Python3. You upload two images: **style** 
+and **content**, bot send you a content-image with style from style-image.
+
+## How to use
+
+1. Send /start to @refresher_jackssnbot
+2. Send style-photo with caption "Style"
+3. Send content-photo with caption "Photo"
+4. Waiting for result-photo (it takes about 5 minutes)
+
+## How to deploy
+
+1. Clone this repo
+2. Create file `token.txt` with your telegram bot API
+3. Install packages: `pip install -requirements.txt`
+4. Run the script `bot.py`
+
+## About project
+
+It's final project in Deep Learning School p.1 [Spring 2020]
---
+++ bot.py
@@ -0,0 +1,67 @@
+import os
+import logging
+import style_transfer
+
+from aiogram import Bot, Dispatcher, executor, types
+
+with open('token.txt', 'r') as f:
+    API_TOKEN = f.read()
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` command
+    """
+    await message.answer("Hello, <b>%s</b>!\n\nI'm RefresherBot by @jackssn!\n"
+                         "If you want to transfer style from one photo to another send me photo "
+                         "with text \"style\" and than another photo with text \"content\". "
+                         "\n\nAfter that I will send you a result of my work." % message.from_user.first_name,
+                         parse_mode='HTML')
+
+#@dp.message_handler(regexp='style', content_types=types.ContentType.PHOTO)
+#async def cats(message: types.Message):
+#    with open('cat.jpg', 'rb') as photo:
+#        await message.answer_photo(photo, caption='Cats are here 😺')
+
+@dp.message_handler(regexp='style', content_types=types.ContentType.PHOTO)
+async def cats(message: types.Message):
+    style_path = 'data/%s/style' % message.from_user.id
+    if not os.path.exists(style_path):
+        os.mkdir(style_path)
+    await message.photo[-1].download('data/%s/style/%s.jpg' %
+                                     (message.from_user.id, (message.date.strftime('%Y%m%d%H%M%S'))))
+    await message.reply('Style accepted')
+
+@dp.message_handler(regexp='content', content_types=types.ContentType.PHOTO)
+async def cats(message: types.Message):
+    style_path = 'data/%s/style' % message.from_user.id
+    content_path = 'data/%s/content' % message.from_user.id
+    result_path = 'data/%s/result' % message.from_user.id
+    if not os.path.exists(content_path):
+        os.mkdir(content_path)
+    if not os.path.exists(result_path):
+        os.mkdir(result_path)
+    await message.photo[-1].download('data/%s/content/%s.jpg' % (message.from_user.id,
+                                                                 (message.date.strftime('%Y%m%d%H%M%S'))))
+    await message.reply('Content accepted. Now wait a result. It takes about 5 minutes.')
+    last_style = os.listdir(style_path)
+    if last_style:
+        last_style = last_style[-1]
+    else:
+        await message.answer('You are not upload any style-image, so we use default image (picasso).')
+    last_content = os.listdir(content_path)[-1]
+    style_transfer.main(imsize=256, num_steps=150,
+                           img_style=os.path.join(style_path, last_style),
+                           img_content=os.path.join(content_path, last_content))
+    with open(os.path.join(result_path, last_content), 'rb') as photo:
+        await message.answer_photo(photo)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
