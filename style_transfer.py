a2c4d19c6f0ae52d23631d2bb6c5aaef09c53ec5
jackssn <jackssn@github.com>
2020-06-29 16:13:03 +0300

    upd pooling code

--- style_transfer.py
+++ style_transfer.py
@@ -249,7 +249,7 @@ def main(imsize=128, num_steps=200, img_style="images/picasso.jpg", img_content=
         transforms.ToTensor(),
     ])
 
-    output_big = scaler(output[0])
+    output_big = scaler(output[0].cpu())
     img_to_save = output_big.cpu().detach().numpy().transpose(1, 2, 0)
     filepath = img_content.replace('content', 'result')
     plt.imsave(filepath, img_to_save)
